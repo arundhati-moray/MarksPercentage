44f3fc7b9fe490a8c511f196d185857f9adcc9d9
Arundhati Moray <arundhati.moray@instanda.com>
2024-02-26 19:21:55 +0000

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class StudentMarks
+    {
+        public double Maths;
+        public double Science;
+        public double ComputerScience;
+        public double Percentage;
+
+
+        public void CalculatePercentage()
+        {
+            double P = (((Maths + ComputerScience + Science) * 100) / 300);
+            Percentage = P;
+        }
+
+
+
+    }
+}
---
+++ MarksPercentage/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1;
+
+class MarksPercentage
+{
+    static void Main()
+    {
+        StudentMarks student1 = new StudentMarks();
+        StudentMarks student2 = new StudentMarks();
+
+        //initialization
+        student1.Maths = 95;
+        student1.Science = 98;
+        student1.ComputerScience = 100;
+
+        student2.Maths = 96;
+        student2.Science = 99;
+        student2.ComputerScience = 100;
+
+        student1.CalculatePercentage();
+        Console.WriteLine($"Percentage of Student1 is : {student1.Percentage}");
+
+        student1.CalculatePercentage();
+        Console.WriteLine($"Percentage of Student2 is : {student2.Percentage}");
+
+
+    }
+}
